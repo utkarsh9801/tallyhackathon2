594da999ee3240a8207e49987a6bfa887f2b5e4f
utkarsh9801 <utkarshgupta637@gmail.com>
2022-07-16 12:47:57 +0530

    sourav help

---
+++ quiz/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+import quiz.views as quiz_views
+
+urlpatterns = [
+	url(r'^$', quiz_views.qpage),
+	#create a new urk pattern result to the url /quiz/result
+	url(r'^result$', quiz_views.result),
+	
+]
---
+++ quiz/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from quiz.models import Quiz
+
+def qpage(request):
+	questions = Quiz.objects.all()
+
+	return render(request, 'quiz.html', { 'questions': questions})
+
+
+def result(request):
+	return render(request, 'result.html')
+
+	
+	
